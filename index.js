0cd2585e25b8952e2f577b94070b12a105d94615
yinkaenoch <adedokunyinkaenoch@gmail.com>
2022-04-29 23:47:30 +0100

    Feat: add target amount capability to bot

--- index.js
+++ index.js
@@ -5,20 +5,74 @@
  * Fees is settled in the vault collateral asset (ALGO or ASA)
  * */
 
-import { Account, Vault, VAULT_IDS } from "@xbacked-dao/xbacked-sdk";
+import { appendFile } from "fs/promises";
+import {
+  Account,
+  Vault,
+  VAULT_IDS,
+  convertFromMicroUnits,
+} from "@xbacked-dao/xbacked-sdk";
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const feeCollector = async ({ account, vault }) => {
-  console.log(`Collecting fees... ${new Date().toString()}`);
-  const isFeesSettled = await account.collectFees({ vault });
+const feeCollector = async ({ account, vault, targetAmount = 0 }) => {
+  try {
+    const { accruedFees } = await account.getVaultState({ vault });
 
-  if (!isFeesSettled) return false;
+    // Early exit if there is no fees to collect
+    if (accruedFees == 0) {
+      console.log(
+        "Accrued fee is zero (0). There is no fees to settle! Trying again..."
+      );
+      return false;
+    }
 
-  isFeesSettled && console.log("Fee was successfully settled");
+    // If targetAmount is specified
+    if (targetAmount > 0) {
+      console.log("Checking if target amount >= accrued fees");
+
+      if (convertFromMicroUnits(accruedFees) < targetAmount) {
+        console.log(
+          `Accrued fees: ${convertFromMicroUnits(
+            accruedFees
+          )} is lesser than targetAmount: ${targetAmount}`
+        );
+
+        return false;
+      }
+    }
+
+    console.log("Collecting fees...");
+    const isFeesSettled = await account.collectFees({ vault });
+
+    if (!isFeesSettled) return false;
+
+    isFeesSettled && console.log("Fee was successfully settled");
+
+    // Log settlement
+    const settlement = `${new Date().toString()} \n Fees Accrued: ${convertFromMicroUnits(
+      accruedFees
+    )} \n Reward: ${convertFromMicroUnits((accruedFees * 0.5) / 100)} \n\n`;
+
+    // Store log in log
+    console.log("Logging settlement...");
+    appendFile("settlement_log", settlement, "utf-8", (err) => {
+      if (err) {
+        console.log(`Failed to log settlement.`);
+        throw err;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-const taskRunner = async ({ sleepDuration, network, passPhrase }) => {
+const taskRunner = async ({
+  sleepDuration,
+  network,
+  passPhrase,
+  targetAmount,
+}) => {
   // Check for null parameters
   if (!sleepDuration || !network || !passPhrase)
     throw new Error("Some required parameters are missing!");
@@ -38,7 +92,7 @@ const taskRunner = async ({ sleepDuration, network, passPhrase }) => {
     try {
       console.log("Sleeping...");
       await sleep(sleepDuration);
-      await feeCollector({ account, vault });
+      await feeCollector({ account, vault, targetAmount });
     } catch (e) {
       console.log(e);
     }
--- runner.js
+++ runner.js
@@ -5,4 +5,5 @@ taskRunner({
   sleepDuration: process.env.SLEEP_DURATION,
   network: process.env.NETWORK,
   passPhrase: process.env.PASS_PHRASE,
+  targetAmount: process.env.TARGET_AMOUNT,
 }).catch((e) => console.log(e));
